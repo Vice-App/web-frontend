00973f5d6dc2c034daf34a010b51cc1343ff07f1
Aymeric @ Vice App <aymeric@vice.cool>
2016-03-27 01:27:06 +0700

    Solving issue #5 #6 #7 Split Step 3 in 2 different steps Add text for Step 3 & Step 4 Change Step progress display Create VN version of 1 pager Delete old registration steps pages.

--- js/regprog.js
+++ js/regprog.js
@@ -10,10 +10,10 @@ $('.next').click(function(){
           
           //update progress
           var step = $(e.target).data('step');
-          var percent = (parseInt(step) / 3) * 100;
+          var percent = (parseInt(step) / 4) * 100;
           
           $('.progress-bar').css({width: percent + '%'});
-          $('.progress-bar').text("Step " + step + " of 3");
+          $('.progress-bar').text("Step " + step + " of 4");
           
           //e.relatedTarget // previous tab
           
